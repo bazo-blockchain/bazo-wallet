dfb6663481111840ecd878b312914b2d53509c10
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-04-05 12:06:09 +0200

    create routing example

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="hello">
+	<router-link :to="{name: 'Test'}">Go To Test</router-link>
 	<h1>{{ msg }}</h1>
 	<h2>Essential Links</h2>
 	<ul>
---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="test">
+	hallöchen {{ test123 }}
+</div>
+</template>
+
+<script>
+export default {
+	name: 'test',
+	data: function () {
+		return {
+			test123: 'test321'
+		}
+	}
+};
+</script>
+
+<style>
+</style>
