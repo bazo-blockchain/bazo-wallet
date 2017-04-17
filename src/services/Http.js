b418ad542656a20554bc56426763de4cb341ab94
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-04-17 16:14:07 +0200

    adapt front-end to new REST interfaces

--- src/services/Http.js
+++ src/services/Http.js
@@ -6,17 +6,17 @@ const DO_NOT_INTERCEPT = { 'DO_NOT_INTERCEPT': 'enabled' };
 const Http = {
 
 	getUser: function (doNotIntercept) {
-		return Vue.http.get(HOST + '/v1/user/auth/get',
+		return Vue.http.get(HOST + '/auth/common/user-account',
 			doNotIntercept ? { headers: DO_NOT_INTERCEPT } : undefined);
 	},
 
 	login: function (credentials, doNotIntercept) {
-		return Vue.http.post(HOST + '/user/login', credentials,
+		return Vue.http.post(HOST + '/user-account/login', credentials,
 			doNotIntercept ? { headers: DO_NOT_INTERCEPT } : undefined);
 	},
 
 	adminGetAccounts: function () {
-		return Vue.http.get(HOST + '/admin/accounts');
+		return Vue.http.get(HOST + '/auth/admin/accounts');
 	}
 
 };
