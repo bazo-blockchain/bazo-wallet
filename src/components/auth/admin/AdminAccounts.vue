f60f0e4a20f567ec379ed799260ba07c5e32d06e
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-04-19 18:39:11 +0200

    improve accounts view

--- src/components/auth/admin/AdminAccounts.vue
+++ src/components/auth/admin/AdminAccounts.vue
@@ -1,51 +1,59 @@
 <template>
 	<div class="admin-accounts">
 		<div class="container">
-			<h1>{{ $t('adminAccounts.title') }}</h1>
-
-			<div v-if="items.length > 0">
-
-				<div class="justify-content-centermy-1 row">
-					<b-form-fieldset horizontal label="Rows per page" class="col-6" :label-size="6">
-						<b-form-select :options="[{text:10,value:10},{text:20,value:20},{text:50,value:50}]" v-model="perPage">
-						</b-form-select>
-					</b-form-fieldset>
-					<b-form-fieldset horizontal label="Search" class="col-6" :label-size="2">
-						<b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
-					</b-form-fieldset>
-				</div>
+			<h1 class="display-4">{{ $t('adminAccounts.title') }}</h1>
+			<hr>
+			<div v-if="!isLoading">
+				<div v-if="items.length > 0">
 	
-				<b-table striped hover :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter">
-					<template slot="timeCreated" scope="item">
-						{{ item.value | moment("YYYY-MM-DD, HH:mm:ss") }}
-					</template>
-					<template slot="privateKeyServer" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="publicKeyServer" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="publicKeyClient" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="satoshiBalance" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="virtualBalance" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="totalBalance" scope="item">
-						{{ item.value }}
-					</template>
-					<template slot="actions" scope="item">
-						<b-btn size="sm" @click="details(item.item)">Details</b-btn>
-					</template>
-				</b-table>
+					<div class="justify-content-centermy-1 row">
+						<b-form-fieldset horizontal label="Rows per page" class="col-6" :label-size="6">
+							<b-form-select :options="[{text:10,value:10},{text:20,value:20},{text:50,value:50}]" v-model="perPage">
+							</b-form-select>
+						</b-form-fieldset>
+						<b-form-fieldset horizontal label="Search" class="col-6" :label-size="2">
+							<b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+						</b-form-fieldset>
+					</div>
+		
+					<b-table striped hover :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter">
+						<template slot="timeCreated" scope="item">
+							{{ item.value | moment("YYYY-MM-DD, HH:mm:ss") }}
+						</template>
+						<template slot="privateKeyServer" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="publicKeyServer" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="publicKeyClient" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="satoshiBalance" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="virtualBalance" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="totalBalance" scope="item">
+							{{ item.value }}
+						</template>
+						<template slot="actions" scope="item">
+							<b-btn size="sm" @click="details(item.item)">Details</b-btn>
+						</template>
+					</b-table>
+		
+					<div class="justify-content-center row my-1">
+						<b-pagination size="md" :total-rows="this.items.length" :per-page="perPage" v-model="currentPage" />
+					</div>
 	
-				<div class="justify-content-center row my-1">
-					<b-pagination size="md" :total-rows="this.items.length" :per-page="perPage" v-model="currentPage" />
 				</div>
-
+				<div v-else>
+					<div class="alert alert-warning">
+						<b>{{ $t('adminAccounts.noEntryAvailableTitle') }}</b>
+						{{ $t('adminAccounts.noEntryAvailable') }}
+					</div>
+				</div>
 			</div>
 
 		</div>
@@ -59,6 +67,7 @@ export default {
 	name: 'admin-accounts',
 	data: () => {
 		return {
+			isLoading: false,
 			items: [],
 			fields: {
 				timeCreated: {
@@ -105,9 +114,14 @@ export default {
 			alert(JSON.stringify(item));
 		},
 		updateData: function () {
+			this.isLoading = true;
+
 			Http.adminGetAccounts()
 				.then((response) => {
 					this.items = response.body;
+					this.isLoading = false;
+				}, () => {
+					this.isLoading = false;
 				});
 		}
 	},
@@ -115,12 +129,16 @@ export default {
 		messages: {
 			en: {
 				adminAccounts: {
-					title: 'Account overview'
+					title: 'Account overview',
+					noEntryAvailableTitle: 'Attention:',
+					noEntryAvailable: 'There is currently no account available.'
 				}
 			},
 			de: {
 				adminAccounts: {
-					title: 'Kontoübersicht'
+					title: 'Kontoübersicht',
+					noEntryAvailableTitle: 'Achtung:',
+					noEntryAvailable: 'Derzeit ist kein Konto verfügbar.'
 				}
 			}
 		}
