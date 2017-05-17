09e62ff46e2c269f42f1f881f4ac254e5334e947
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-05-18 00:23:00 +0200

    display channel transaction

--- src/components/auth/admin/AdminAccountsDetail.vue
+++ src/components/auth/admin/AdminAccountsDetail.vue
@@ -71,7 +71,7 @@
 							<th scope="row">{{ $t('adminAccountsDetail.fields.channelTransaction') }}</th>
 							<td>
 								<span v-if="accountDetail.account.channelTransaction">
-									{{ $t('adminAccountsDetail.seeBelow') }} <i class="fa fa-arrow-down"></i>
+									{{ $t('adminAccountsDetail.channelTransaction', channelTransaction) }}
 								</span>
 								<span v-else>
 									<i class="fa fa-minus"></i>
@@ -92,41 +92,6 @@
 						</tr>
 					</tbody>
 				</table>
-				<div v-if="accountDetail.account.channelTransaction">
-					<h2 class="display-7 channel-transaction-title">{{ $t('adminAccountsDetail.channelTransaction.title') }}</h2>
-					<div class="row">
-						<div class="col-md-6">
-							<h3 class="display-8">{{ $t('adminAccountsDetail.channelTransaction.inputTitle') }}</h3>
-							<table class="table table-striped">
-								<tr>
-									<th>Hash</th>
-									<th>Index</th>
-									<th>Script</th>
-									<th>Sequence</th>
-								</tr>
-								<tr v-for="input in channelTransaction.ins">
-									<td>{{ input.hash }}</td>
-									<td>{{ input.index }}</td>
-									<td>{{ input.script }}</td>
-									<td>{{ input.sequence }}</td>
-								</tr>
-							</table>
-						</div>
-						<div class="col-md-6">
-							<h3 class="display-8">{{ $t('adminAccountsDetail.channelTransaction.outputTitle') }}</h3>
-							<table class="table table-striped">
-								<tr>
-									<th>Script</th>
-									<th>Value</th>
-								</tr>
-								<tr v-for="output in channelTransaction.outs">
-									<td>{{ output.script }}</td>
-									<td>{{ output.value }}</td>
-								</tr>
-							</table>
-						</div>
-					</div>
-				</div>
 				<h2 class="display-7 time-locked-addresses-title">{{ $t('adminAccountsDetail.timeLockedAddresses') }}:</h2>
 				<div v-if="accountDetail.timeLockedAddresses && accountDetail.timeLockedAddresses.length > 0">
 					<b-table striped hover :items="accountDetail.timeLockedAddresses" :fields="timeLockedAddressesFields">
@@ -191,16 +156,19 @@ export default {
 	},
 	computed: {
 		channelTransaction: function () {
-			// TODO use commented code for bitcoin transaction
-			// make sure to also remove the fake channel transaction in load data
-
 			if (window.bitcoin) {
-				return window.bitcoin.Transaction.fromHex('0100000002d8c8df6a6fdd2addaf589a83d860f18b44872d13ee6ec3526b2b470d42a96d4d000000008b483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7abffffffff2ab3fa4f68a512266134085d3260b94d3b6cfd351450cff021c045a69ba120b2000000008b4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226ffffffff02c0175302000000001976a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788acc01c3d09000000001976a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac00000000');
-			} else {
+				var tx = window.bitcoin.Transaction.fromHex('0100000002d8c8df6a6fdd2addaf589a83d860f18b44872d13ee6ec3526b2b470d42a96d4d000000008b483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7abffffffff2ab3fa4f68a512266134085d3260b94d3b6cfd351450cff021c045a69ba120b2000000008b4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226ffffffff02c0175302000000001976a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788acc01c3d09000000001976a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac00000000');
 				return {
-					ins: [],
-					outs: []
+					version: tx.version,
+					numInputs: tx.ins.length,
+					numOutputs: tx.outs.length,
+					outputs: tx.outs.map(out => {
+						return out.value;
+					}).join(' satoshi, ') + ' satoshi',
+					raw: tx
 				};
+			} else {
+				return null;
 			}
 			/* if (window.bitcoin && this.accountDetail && this.accountDetail.account && this.accountDetail.account.channelTransaction) {
 				return Bitcoin.Transaction.fromHex(this.accountDetail.account.channelTransaction); */
@@ -275,11 +243,7 @@ export default {
 					locked: 'Locked',
 					notLocked: 'Not Locked',
 					seeBelow: 'See below',
-					channelTransaction: {
-						title: 'Channel Transaction',
-						inputTitle: 'Input',
-						outputTitle: 'Output'
-					},
+					channelTransaction: 'Version {version}; {numInputs} Inputs, {numOutputs} Outputs ({outputs})',
 					fields: {
 						publicKeyClient: 'Public Key Client',
 						privateKeyServer: 'Private Key Server',
@@ -311,11 +275,7 @@ export default {
 					locked: 'Gesperrt',
 					notLocked: 'Nicht gesperrt',
 					seeBelow: 'Siehe unten',
-					channelTransaction: {
-						title: 'Channel Transaktion',
-						inputTitle: 'Eingang',
-						outputTitle: 'Ausgang'
-					},
+					channelTransaction: 'Version {version}; {numInputs} Eingänge, {numOutputs} Ausgänge ({outputs})',
 					fields: {
 						publicKeyClient: 'Public Key Client',
 						privateKeyServer: 'Private Key Server',
@@ -347,8 +307,7 @@ export default {
 	font-size: 70%;
 	margin-bottom: -5px;
 }
-.time-locked-addresses-title,
-.channel-transaction-title {
+.time-locked-addresses-title {
 	margin-top: 40px;
 	margin-bottom: 20px;
 }
