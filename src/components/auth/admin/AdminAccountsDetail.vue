d338b7845b2891e568098b26fc41ad106699702e
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-06-13 13:22:27 +0200

    show correct bitcoin channel transaction in detail view (remove mock)

--- src/components/auth/admin/AdminAccountsDetail.vue
+++ src/components/auth/admin/AdminAccountsDetail.vue
@@ -156,22 +156,26 @@ export default {
 	},
 	computed: {
 		channelTransaction: function () {
-			if (window.bitcoin) {
-				var tx = window.bitcoin.Transaction.fromHex('0100000002d8c8df6a6fdd2addaf589a83d860f18b44872d13ee6ec3526b2b470d42a96d4d000000008b483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7abffffffff2ab3fa4f68a512266134085d3260b94d3b6cfd351450cff021c045a69ba120b2000000008b4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226ffffffff02c0175302000000001976a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788acc01c3d09000000001976a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac00000000');
-				return {
-					version: tx.version,
-					numInputs: tx.ins.length,
-					numOutputs: tx.outs.length,
-					outputs: tx.outs.map(out => {
-						return out.value;
-					}).join(' satoshi, ') + ' satoshi',
-					raw: tx
-				};
+			// test channel transaction:
+			// var tx = window.bitcoin.Transaction.fromHex('0100000002d8c8df6a6fdd2addaf589a83d860f18b44872d13ee6ec3526b2b470d42a96d4d000000008b483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7abffffffff2ab3fa4f68a512266134085d3260b94d3b6cfd351450cff021c045a69ba120b2000000008b4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226ffffffff02c0175302000000001976a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788acc01c3d09000000001976a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac00000000');
+			if (window.bitcoin && this.accountDetail && this.accountDetail.account && this.accountDetail.account.channelTransaction) {
+				var tx = window.bitcoin.Transaction.fromHex(this.accountDetail.account.channelTransaction);
+				if (tx) {
+					return {
+						version: tx.version,
+						numInputs: tx.ins.length,
+						numOutputs: tx.outs.length,
+						outputs: tx.outs.map(out => {
+							return out.value;
+						}).join(' satoshi, ') + ' satoshi',
+						raw: tx
+					};
+				} else {
+					return null;
+				}
 			} else {
 				return null;
 			}
-			/* if (window.bitcoin && this.accountDetail && this.accountDetail.account && this.accountDetail.account.channelTransaction) {
-				return Bitcoin.Transaction.fromHex(this.accountDetail.account.channelTransaction); */
 		},
 		dateFormat: function () {
 			return Util.DATE_FORMAT;
@@ -220,10 +224,6 @@ export default {
 			this.isLoading = true;
 			Http.Auth.Admin.getAccount(this.publicKeyClient).then((response) => {
 				this.accountDetail = response.body;
-
-				// TODO remove the following line!!
-				this.accountDetail.account.channelTransaction = true;
-
 				this.isLoading = false;
 			}, () => {
 				this.isLoading = false;
