d96742f472ce3bb0ab7a61bcbc94b1c6bb1a1cf0
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-06-23 17:32:22 +0200

    add compression to start-server script

--- start-server.js
+++ start-server.js
@@ -1,13 +1,22 @@
+// starts a very simple node webserver in the dist folder
+
 var fs = require("fs");
 var host = "127.0.0.1";
 var port = 7070;
 var express = require("express");
+var compression = require("compression");
 
 var app = express();
+app.use(compression({filter: function (req, res) {
+	if (req.headers['x-no-compression']) {
+		return false;
+	}
+	return compression.filter(req, res);
+}}));
 app.use(express.static(__dirname + "/dist"));
 
-app.get("/", function(request, response) { //root dir
-    response.send("Hello!!");
+app.get("/", function(request, response) {
+	response.send("started on http://" + host + ":" + port);
 });
 
 app.listen(port, host);
