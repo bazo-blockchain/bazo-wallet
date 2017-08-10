79d0fe4a01dc5fedb2ad0edb25e9a013db01c6e7
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2017-08-10 12:12:37 +0200

    add environments to properties.js

--- src/properties.js
+++ src/properties.js
@@ -1,23 +1,41 @@
 ﻿import Bitcoin from 'coinblesk-frontend-bitcoinjs';
 
-const Properties = {
-	// possible values: Bitcoin.networks.bitcoin, Bitcoin.networks.testnet
-	BITCOIN_NETWORK: Bitcoin.networks.testnet,
-
-	// server address
-	// HOST: getCurrentHost() + ':8080'
-	HOST: getCurrentHost()
-};
-
+// possible entries are 'DEV', 'TEST' and 'PROD'
+const Properties = getProperties('DEV2');
 export default Properties;
 
+// testnet works also for pregtest
+function getProperties (env) {
+	if (env === 'DEV') {
+		return {
+			ENVIRONMENT: env,
+			BITCOIN_NETWORK: Bitcoin.networks.testnet,
+			HOST: getCurrentHost() + ':8080'
+		};
+	} else if (env === 'TEST') {
+		return {
+			ENVIRONMENT: env,
+			BITCOIN_NETWORK: Bitcoin.networks.testnet,
+			HOST: 'https://bitcoin2-test.csg.uzh.ch/coinblesk-server'
+		};
+	} else if (env === 'PROD') {
+		return {
+			ENVIRONMENT: env,
+			BITCOIN_NETWORK: Bitcoin.networks.bitcoin,
+			// TODO enter a correct host here:
+			HOST: 'https://bitcoin.csg.uzh.ch/coinblesk-server'
+		};
+	} else {
+		throw new Error('Coinblesk Error: An invalid environment is set. Set a correct one in coinblesk-frontend/src/properties.js');
+	}
+}
+
 function getCurrentHost () {
-	/* let protocol = window.location.protocol;
+	let protocol = window.location.protocol;
 	if (/[^:]$/.test(protocol)) {
 		protocol += ':';
 	}
 	let hostname = window.location.hostname;
 
-	return protocol + '//' + hostname; */
-	return 'https://bitcoin2-test.csg.uzh.ch/coinblesk-server'
+	return protocol + '//' + hostname;
 }
