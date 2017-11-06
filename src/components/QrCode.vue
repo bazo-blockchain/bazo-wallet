d0a767ccbbad4ebb6e404e63882f2966d5e2614a
Jan von der Assen <jan.vda@gmx.ch>
2017-11-06 18:40:50 +0100

    Adapt QrCode component for UserRequest

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -1,11 +1,6 @@
 <template>
 <div class="qr-code-wrapper">
-	<div class="qr-fade-out" @click.prevent="shown = false" :class="{ 'shown': shown }"></div>
-	<i class="fa fa-qrcode increase-focus" @click.prevent="shown = !shown"></i>
 	<div class="qr-image" :class="{ 'shown': shown }">
-		<a class="close increase-focus" @click.prevent="shown = false">
-			<i class="fa fa-times"></i>
-		</a>
 		<div></div>
 	</div>
 </div>
@@ -82,59 +77,29 @@ export default {
 	transition: 0.2s ease all;
 	opacity: 0;
 	visibility: hidden;
-	
+
 	&.shown {
 		opacity: 1;
 		visibility: visible;
 	}
 }
 .qr-image {
-	position: absolute;
-	bottom: 2.3em;
-	left: 50%;
-	transform: translateX(-50%);
 	background: white;
 	box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
 	border-radius: 5px;
 	padding: 12px;
 	z-index: 101;
-	opacity: 0;
-	visibility: hidden;
 	transition: 0.2s ease all;
 	z-index: 103;
-	
-	&.shown {
-		opacity: 1;
-		visibility: visible;
-	}
-	
-	&:after {
-		content: "";
-		display: block;
-		transform: translateX(-50%) rotate(45deg);
-		width: 1em;
-		height: 1em;
-		position: absolute;
-		bottom: -0.5em;
-		background: white;
-		left: 50%;
-	}
-	
-	/deep/ svg {
-		width: 164px;
-		height: 164px;
-		path {
-			transform: scale(2);
-		}
-	}
-	
-	.close {
-		display: block;
-		position: absolute;
-		right: -1.1em;
-		top: 0.1em;
-		font-size: 24px;
-		color: black;
-	}
+
+
+	// /deep/ svg {
+	// 	width: 500px;
+	// 	height: 500px;
+	// 	path {
+	// 		transform: scale(2);
+	// 	}
+	// }
+
 }
 </style>
