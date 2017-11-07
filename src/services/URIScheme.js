2b9bf34ab988ffbcf4f4e15405ed63db445620fb
Jan von der Assen <jan.vda@gmx.ch>
2017-11-07 13:42:59 +0100

    Add adapted BIP21 library to encode/decode payment information into a model similar to BIP21 for Bitcoin.

---
+++ src/services/URIScheme.js
@@ -0,0 +1,34 @@
+import qs from 'qs';
+// bazo:<address>[?amount=<amount>][?label=<label>][?message=<message>]
+
+const URIScheme = {
+  decode: function decode (uri) {
+    var qregex = /bazo:\/?\/?([^?]+)(\?([^]+))?/.exec(uri)
+    if (!qregex) throw new Error('Invalid URI: ' + uri)
+
+    var address = qregex[1]
+    var query = qregex[3]
+    var options = qs.parse(query)
+
+    if (options.amount) {
+      options.amount = Number(options.amount)
+      if (!isFinite(options.amount)) throw new Error('Invalid amount')
+      if (options.amount < 0) throw new Error('Invalid amount')
+    }
+
+    return { address: address, options: options }
+  },
+  encode: function encode (address, options) {
+    options = options || {}
+    var query = qs.stringify(options)
+
+    if (options.amount) {
+      if (!isFinite(options.amount)) throw new TypeError('Invalid amount')
+      if (options.amount < 0) throw new TypeError('Invalid amount')
+    }
+
+    return 'bazo:' + address + (query ? '?' : '') + query
+  }
+};
+
+export default URIScheme;
