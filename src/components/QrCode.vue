dd00e9ed63054b9a3ac9c25b2e3869de5b4182e0
Jan von der Assen <jan.vda@gmx.ch>
2017-11-07 14:41:58 +0100

    Create new qr code upon each data change.

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -35,26 +35,38 @@ export default {
 			let qr = null;
 			// size is depending on the input length
 			// an exception is thrown, if the size is not large enough
-			try {
-				qr = makeQR(1);
-			} catch (e) {
-				try {
-					qr = makeQR(2);
-				} catch (e) {
-					try {
-						qr = makeQR(3);
-					} catch (e) {
-						try {
-							qr = makeQR(4);
-						} catch (e) {
-							qr = makeQR(5);
-						}
-					}
-				}
-			}
+			// try {
+			// 	qr = makeQR(1);
+			// } catch (e) {
+			// 	try {
+			// 		qr = makeQR(2);
+			// 	} catch (e) {
+			// 		try {
+			// 			qr = makeQR(3);
+			// 		} catch (e) {
+			// 			try {
+			// 				qr = makeQR(4);
+			// 			} catch (e) {
+			// 				qr = makeQR(5);
+			// 			}
+			// 		}
+			// 	}
+			// }
+      try {
+        qr = makeQR(5)
+      } catch (e) {
+
+      } finally {
+
+      }
 			this.$el.querySelector('.qr-image div').innerHTML = qr.createImgTag(5, 0);
 		}
-	}
+	},
+  watch: {
+    content: function (newVal) {
+      this.createQrImage();
+    }
+  }
 };
 </script>
 
