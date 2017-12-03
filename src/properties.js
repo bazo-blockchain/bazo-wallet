68a7ae5e3bed6c969bd8219dc4e78966bab5fcc0
Jan von der Assen <jan.vda@gmx.ch>
2017-12-03 19:21:05 +0100

    Reconfigure properties.

--- src/properties.js
+++ src/properties.js
@@ -1,5 +1,3 @@
-﻿import Bitcoin from 'coinblesk-frontend-bitcoinjs';
-
 // possible entries are 'DEV', 'TEST' and 'PROD'
 const Properties = getProperties('DEV');
 export default Properties;
@@ -8,22 +6,18 @@ function getProperties (env) {
 	if (env === 'DEV') {
 		return {
 			ENVIRONMENT: env,
-			// Bitcoin.networks.testnet works also for regtest
-			BITCOIN_NETWORK: Bitcoin.networks.testnet,
-			HOST: getCurrentHost() + ':8080'
+			HOST: getCurrentHost() + ':8001'
 		};
 	} else if (env === 'TEST') {
 		return {
 			ENVIRONMENT: env,
-			BITCOIN_NETWORK: Bitcoin.networks.testnet,
 			HOST: 'https://bitcoin2-test.csg.uzh.ch/coinblesk-server'
 		};
 	} else if (env === 'PROD') {
 		return {
 			ENVIRONMENT: env,
-			BITCOIN_NETWORK: Bitcoin.networks.bitcoin,
 			// TODO enter a correct host here:
-			HOST: 'https://bitcoin.csg.uzh.ch/coinblesk-server'
+			HOST: 'TBD'
 		};
 	} else {
 		throw new Error('Coinblesk Error: An invalid environment is set. Set a correct one in coinblesk-frontend/src/properties.js');
