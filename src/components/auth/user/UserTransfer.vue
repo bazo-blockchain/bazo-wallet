5b007050cd2cbb82d0f002fdd0aeca57aad5de76
Jan von der Assen <jan.vda@gmx.ch>
2017-12-04 15:14:11 +0100

    Adapt logic in User-Transfer for client side encryption.

--- src/components/auth/user/UserTransfer.vue
+++ src/components/auth/user/UserTransfer.vue
@@ -1,17 +1,18 @@
 <template>
 <div>
-	<b-modal :id="'user-transfer' + (onlyUnlock ? '-unlock' : '') + (separate ? '-' + separate : '')" :title="!onlyUnlock ? $t('userTransfer.title') : $t('userTransfer.titleUnlock')"
+	<b-modal :id="'user-transfer'" :title="$t('userTransfer.title')"
 		size="md" :hide-footer="true" @hidden="modalWasClosed">
 		<div>
-			<div class="alert alert-info" v-html="$t('userTransfer.description', { amount })" v-if="!onlyUnlock"></div>
+			<div class="alert alert-info" v-html="$t('userTransfer.description', { amount })"></div>
 			<b-form-fieldset :label="$t('userTransfer.passPhrase')">
-				<b-form-input type="password" v-model="passPhrase" @input="formIsTouched = false" @keyup.enter="submit" :class="{ 'form-error': !validPassPhrase && formIsTouched }"></b-form-input>
+				<b-form-input type="password" v-model="privateKey" @input="formIsTouched = false" @keyup.enter="submit" :class="{ 'form-error': !validPassPhrase && formIsTouched }"></b-form-input>
 			</b-form-fieldset>
 			<div class="error-description" v-if="formIsTouched && !validPassPhrase">
 				<i class="fa fa-warning"></i>
 				{{ $t('userTransfer.errorDescription') }}
 			</div>
-			<b-button variant="primary" class="pull-right" @click.prevent="submit">{{ !onlyUnlock ? $t('userTransfer.submit') : $t('userTransfer.submitUnlock') }}</b-button>
+      hash: {{this.transactionHash}}
+			<b-button variant="primary" class="pull-right" @click.prevent="submit">{{ $t('userTransfer.submit') }}</b-button>
 			<b-button variant="default" class="pull-right" @click.prevent="hideModal">{{ $t('userTransfer.cancel') }}</b-button>
 		</div>
 	</b-modal>
@@ -19,44 +20,58 @@
 </template>
 
 <script>
-import CryptoService from '@/services/CryptoService';
+import elliptic from 'elliptic';
+import HttpService from '@/services/HttpService';
 
 export default {
 	name: 'user-transfer',
 	data: function () {
 		return {
-			passPhrase: '',
+			privateKey: '',
 			validPassPhrase: false,
-			formIsTouched: false
+			formIsTouched: false,
+      // eslint-disable-next-line
+      ec: new elliptic.ec('p256')
 		}
 	},
 	props: {
-		encryptedPrivateKey: String,
-		amount: Number,
-		onlyUnlock: Boolean,
-		separate: String
+		transactionHash: String,
+		amount: Number
 	},
 	methods: {
 		submit: function () {
+      let that = this;
 			this.formIsTouched = true;
 			try {
-				const privateKeyWif = CryptoService.decrypt(this.passPhrase, this.encryptedPrivateKey);
 				this.validPassPhrase = true;
-				this.$emit('private-key-decrypted', privateKeyWif);
-				this.hideModal();
+				// this.$emit('private-key-decrypted', privateKeyWif);
+        let key = this.ec.keyFromPrivate(this.privateKey)
+        let sig = key.sign(this.transactionHash);
+        let result = '';
+        result = sig.r.toJSON() + sig.s.toJSON();
+
+        HttpService.sendSignedFundsTx(this.transactionHash, result).then(() => {
+          that.$toasted.global.success(this.$t('userTransfer.sendSuccess'));
+          that.hideModal();
+        }).catch(() => {
+          that.$toasted.global.warn(this.$t('userTransfer.sendError'));
+          that.hideModal();
+        });
 			} catch (e) {
 				this.validPassPhrase = false;
 			}
 		},
 		hideModal: function () {
-			this.$root.$emit('hide::modal', 'user-transfer' + (this.onlyUnlock ? '-unlock' : '') + (this.separate ? '-' + this.separate : ''));
+			this.$root.$emit('hide::modal', 'user-transfer');
 		},
 		modalWasClosed: function () {
-			this.passPhrase = '';
-			this.formIsTouched = false;
-			this.validPassPhrase = false;
+			this.privateKey = '';
+			// this.validPassPhrase = false;
 		}
 	}
+  // mounted: function () {
+  //   console.log('mounted', this.transactionHash, this.amount);
+  // }
 };
 </script>
 
@@ -81,25 +96,25 @@ export default {
 	"en": {
 		"userTransfer": {
 			"title": "Confirm your payment",
-			"titleUnlock": "Unlock your passphrase",
-			"passPhrase": "Pass Phrase",
+			"passPhrase": "Private Key",
 			"submit": "Transfer",
-			"submitUnlock": "Submit",
 			"cancel": "Cancel",
 			"errorDescription": "This pass phrase is incorrect. Please try again.",
-			"description": "You are about to transfer <b>{amount} BTC</b>. Please enter your pass phrase to finish the transaction process."
-		}
+			"description": "You are about to transfer <b>{amount} Bazo coins</b>. Please enter your private to sign the transaction and finish the transaction process. Your private key will <b>not</b> get sent over the network.",
+      "sendError": "The transaction could not be sent to the Bazo network.",
+      "sendSuccess": "The transaction was successfully sent to the Bazo network."
+    }
 	},
 	"de": {
 		"userTransfer": {
 			"title": "Zahlung bestätigen",
-			"titleUnlock": "Passphrase entsperren",
-			"passPhrase": "Passphrase",
+			"passPhrase": "Privater Schlüssel",
 			"submit": "Überweisen",
-			"submitUnlock": "Abschicken",
 			"cancel": "Abbrechen",
 			"errorDescription": "Diese Pass Phrase ist ungültig. Bitte versuchen Sie es erneut.",
-			"description": "Sie sind im Begriff <b>{amount} BTC</b> zu überweisen. Geben Sie bitte Ihre Passphrase ein, um die Transaktion abzuschliessen."
+			"description": "Sie sind im Begriff <b>{amount} Bazo coins</b> zu überweisen. Geben Sie bitte Ihren privaten Schlüssel ein, um die Transaktion zu signieren und abzuschliessen. Der Schlüssel wird dabei nicht versandt.",
+      "sendError": "Die Transaktion konnte nicht ins Bazo Netzwerk gesendet werden.",
+      "sendSuccess": "Die Transaktion wurde erfolgreich <b>versendet</b>."
 		}
 	}
 }
