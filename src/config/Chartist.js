29848516570dadd5593c4d979511654e492b1c8f
Jan von der Assen <jan.vda@gmx.ch>
2017-12-12 12:33:09 +0100

    Delete unused config files and adapt dependent configurations

--- src/config/Chartist.js
+++
@@ -1,4 +0,0 @@
-import Chartist from 'chartist/dist/chartist.js';
-import 'chartist/dist/chartist.css';
-
-export default Chartist;
--- src/config/Store.js
+++ src/config/Store.js
@@ -3,20 +3,12 @@ import Vuex from 'vuex';
 import PersistedState from 'vuex-persistedstate'
 import HttpService from '@/services/HttpService';
 import Translation from '@/config/Translation';
-import Router from '@/config/Router';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	state: {
 		language: null,
-		user: null,
-		userBalance: null,
-		auth: {
-			token: null,
-			authenticated: false,
-			role: null
-		},
     config: {
       configured: false,
       accounts: [],
@@ -61,20 +53,6 @@ const store = new Vuex.Store({
   },
 	// should be private:
 	mutations: {
-		updateUser: function (state, user) {
-			user.lastUpdate = new Date();
-			state.user = user;
-		},
-		clearUser: function (state) {
-			state.user = null;
-		},
-		updateUserBalance: function (state, userBalance) {
-			userBalance.lastUpdate = new Date();
-			state.userBalance = userBalance;
-		},
-		clearUserBalance: function (state) {
-			state.userBalance = null;
-		},
 		updateLanguage: function (state, language) {
 			state.language = language;
 		},
@@ -121,63 +99,24 @@ const store = new Vuex.Store({
     setCustomURL: function (state, url) {
       state.settings.customURL = url;
     },
-		updateAuth: function (state, token) {
-			state.auth.authenticated = true;
-			state.auth.token = token;
-			state.auth.role = (() => {
-				const base64Payload = token.split('.')[1];
-				const payload = JSON.parse(window.atob(base64Payload));
-				return payload.auth;
-			})();
-		},
-		clearAuth: function (state) {
-			state.auth.authenticated = false;
-			state.auth.token = null;
-			state.auth.role = null;
-		},
 		setOffline: function (state, offline) {
 			state.offline = offline;
 		}
 	},
-
 	// should be public:
 	actions: {
 		initialize: function (context) {
-			if (!context.state.offline) {
-				if (context.state.auth.authenticated) {
-					return context.dispatch('updateUser').then(() => {
-						return context.dispatch('updateUserBalance');
-					});
-				} else {
-					context.commit('clearUser');
-					context.commit('clearUserBalance');
-					return Promise.resolve();
-				}
-			}
-		},
-
-		updateUser: function (context) {
-			if (context.state.auth.authenticated) {
-				return HttpService.Auth.getUser(true)
-					.then((response) => {
-						context.commit('updateUser', response.body);
-						return Promise.resolve();
-					}, response => {
-						if (response.status !== 0) {
-							context.dispatch('logout');
-							Vue.toasted.global.warnNoIcon('<i class="fa fa-sign-out"></i>' + Translation.t('toasts.sessionExpired'));
-							Router.push({ path: '/login' });
-
-							return Promise.reject();
-						} else {
-							// offline mode -> do not update
-							return Promise.reject();
-						}
-					});
-			} else {
-				context.commit('clearUser');
-				return Promise.reject();
-			}
+			// if (!context.state.offline) {
+			// 	if (context.state.auth.authenticated) {
+			// 		return context.dispatch('updateUser').then(() => {
+			// 			return context.dispatch('updateUserBalance');
+			// 		});
+			// 	} else {
+			// 		context.commit('clearUser');
+			// 		context.commit('clearUserBalance');
+			// 		return Promise.resolve();
+			// 	}
+			// }
 		},
 		updateUserBalance: function (context, host) {
       context.state.config.accounts.forEach(function (account) {
@@ -190,16 +129,8 @@ const store = new Vuex.Store({
         })
       })
 		},
-
 		updateLanguage: function (context, language) {
 			return context.commit('updateLanguage', language);
-		},
-		updateAuth: function (context, token) {
-			if (typeof token !== 'undefined' && token !== null && token !== '') {
-				return context.commit('updateAuth', token);
-			} else {
-				return context.commit('clearAuth');
-			}
 		},
     updateConfig: function (context, config) {
       return context.commit('updateConfig', config);
@@ -221,11 +152,6 @@ const store = new Vuex.Store({
     },
 		setOffline: function (context, offline) {
 			context.commit('setOffline', !!offline);
-		},
-		logout: function (context) {
-			context.commit('clearUserBalance');
-			context.commit('clearUser');
-			context.commit('clearAuth');
 		}
 	},
 	plugins: [
