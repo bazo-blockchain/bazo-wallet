be6cb209ecf575d0541363e59dbee5f95bf5fd60
Jan von der Assen <jan.vda@gmx.ch>
2018-01-27 20:12:32 +0100

    Change Block Explorer hostname

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -57,7 +57,7 @@
             <template slot="qr" scope="item">
               <div>
                 <b-popover triggers="hover" :content="Translation.t('userAccounts.explorer')" class="popover-element">
-                  <a href="#/auth/user/request" :href="'https://shrouded-dawn-45355.herokuapp.com/account/' + item.item.bazoaddress">
+                  <a href="#/auth/user/request" :href="'https://csg.uzh.ch/bazo/account/' + item.item.bazoaddress">
                     <i class="fa fa-link" aria-hidden="true"></i>
                   </a>
                 </b-popover>
